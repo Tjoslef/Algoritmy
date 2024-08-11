7199cbf26f06365f2546544f50cce835511faac5
Tjoslef <josef.pasek17@gmail.com>
2024-08-11 15:13:58 +0200

    set up

--- Find Common Characters.py
+++
@@ -1,6 +0,0 @@
-import itertools
-import numpy as np
-def kInversePairs(n, k):
-        permutations = list(itertools.permutations(range(n, k)))
-        return permutations
-print(kInversePairs(0,3))
